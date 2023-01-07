391f2cab70f057f7611f87ba18b09b680b1d34fe
ThomasWega <tomous.wega@gmail.com>
2023-01-07 17:04:13 +0100

    moved tablist to core
    Took 8 minutes

--- src/main/java/net/trustgames/lobby/Lobby.java
+++ src/main/java/net/trustgames/lobby/Lobby.java
@@ -10,8 +10,6 @@ import net.trustgames.lobby.hotbar.HotbarListeners;
 import net.trustgames.lobby.spawn.SetSpawnCommand;
 import net.trustgames.lobby.spawn.Spawn;
 import net.trustgames.lobby.spawn.SpawnCommand;
-import net.trustgames.lobby.tablist.TablistHeaderFooter;
-import net.trustgames.lobby.tablist.TablistPrefix;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -43,8 +41,6 @@ public final class Lobby extends JavaPlugin {
         // register events
         EventManager.registerEvent(new Spawn(this), this);
         EventManager.registerEvent(new HotbarListeners(), this);
-        EventManager.registerEvent(new TablistHeaderFooter(this), this);
-        EventManager.registerEvent(new TablistPrefix(), this);
 
         // create config files
         ConfigManager.createConfig(new File(getDataFolder(), "spawn.yml"));
--- src/main/java/net/trustgames/lobby/config/DefaultConfig.java
+++ src/main/java/net/trustgames/lobby/config/DefaultConfig.java
@@ -14,11 +14,6 @@ public class DefaultConfig {
         // settings
         defaultConfig.addDefault("settings.cooldowns.spawn-command-cooldown", 3d);
 
-        // tablist
-        defaultConfig.addDefault("tablist.header", List.of("&f&lTRUSTGAMES &f- &7Chillin' on the hub"));
-        defaultConfig.addDefault("tablist.footer", List.of("&fCheck out &astore.trustgames.net&f for Ranks and Benefits"));
-
-
     }
 }
 
