317755700b5c3ee1c266e0a6ef4d69f4dddd793a
ThomasWega <tomous.wega@gmail.com>
2023-01-12 20:13:36 +0100

    minor improvement
    Took 7 minutes

--- src/main/java/net/trustgames/lobby/hotbar/HotbarItems.java
+++ src/main/java/net/trustgames/lobby/hotbar/HotbarItems.java
@@ -1,13 +1,13 @@
 package net.trustgames.lobby.hotbar;
 
 import net.kyori.adventure.text.Component;
-import net.trustgames.core.managers.InventoryManager;
 import net.trustgames.core.managers.ItemManager;
 import net.trustgames.core.managers.SkullManager;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -18,12 +18,16 @@ import java.util.List;
 
 public class HotbarItems {
 
+    static Inventory inventory;
+
     // list of hotbar items to add
     public static void addItems(Player player) {
 
+        // set the inventory
+        inventory = player.getInventory();
+
         // item flags
         ItemFlag[] itemFlags = new ItemFlag[]{ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_UNBREAKABLE};
-
 
         // server selector
         ItemStack selectorStack = SkullManager.getSkull("eyJ0aW1lc3RhbXAiOjE1MTgxODUxNjg4OTEsInByb2ZpbGVJZCI6IjIzZjFhNTlmNDY5YjQzZGRiZGI1MzdiZmVjMTA0NzFmIiwicHJvZmlsZU5hbWUiOiIyODA3Iiwic2lnbmF0dXJlUmVxdWlyZWQiOnRydWUsInRleHR1cmVzIjp7IlNLSU4iOnsidXJsIjoiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS83YTRiODgzMmFmYzNjZWE4MzIyNGIxNDQxMGI2NjJlNzA3ZThlNzljNjFmNDY2ZTM2MmEzMGU4MmM3ZGU5In19fQ==", "WsGW3iUx8g+xH1NraTlpxgnsp8ClAE40oerBpV+MnPNmBAbZ/WnZwVEI5w9pziuCG75oxuckrO/kyFFHzxivr+OXKoXYwi8jw+8ZRL7v5gappa5nvdbf5kkClNxmxkjDzbN0U01tM91OWIimKwLo5Lel+Zf02vtYw7CF9sM8rowLKAjQzo1Nydb0Hpo2WFsIe4Aa17vyXsHsv4OTDx4Mj1zUvXtFkgTzNCjBuBAfOBaGXp+m6Zo23XJAu010Rf6by0y8OuEm0XHKMY0SXlpE2AepIQDbaMQA2tlViVsDJLAIJJQh0SBILRxC6+8aGdQmC1AG1L7RmtDrIShx6pwF0gGK5BBG0BTtbJ++NtR0sIBk2oNFvm+TtsG/VmIHL6cDiJ7sxvbNJXrr5lp8YJ9Wsgn6P+iJ/qtdSGOfoEQkBmneRZvelbWeUYcpZi8xinoOmgQCDzIWnKZ96T73ArbBZBbeL+6+XGi7lnqv4DMqqfeyzG3YC6SmCzkiBABz5PDxKG0BthnT7Xw0rocBsxHhjWmWC5okskKHZql+Pk1PZnCXWuaC30HxScH11QHy0li4y3J5mclhqISd5/ZbGx/wuOcETQrFKh9M90qObf5VGNxAJovwdIfs3Zh+YCBdYWzsx8UCbLKmewfGWxpat1VgrHmSumE2uojdk81W8lCmyOs=");
@@ -37,9 +41,8 @@ public class HotbarItems {
         skullProfileMeta.setOwningPlayer(player);
 
         // add the items to the inventory
-        InventoryManager.addItemToInventory(player.getInventory(), 0, selectorStack);
-        InventoryManager.addItemToInventory(player.getInventory(), 8, profileStack);
-
+        inventory.setItem(0, selectorStack);
+        inventory.setItem(8, profileStack);
     }
 
     // update the hide item in hotbar
@@ -80,7 +83,7 @@ public class HotbarItems {
                 else{
                     hideStack.setAmount(Bukkit.getOnlinePlayers().size() - 1);
                 }
-                InventoryManager.addItemToInventory(onlinePlayer.getInventory(), 6, hideStack);
+                onlinePlayer.getInventory().setItem(6, hideStack);
             }
         }
     }
