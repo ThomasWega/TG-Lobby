e01389eb21b186bcf53b94af8e693095236ccb6e
ThomasWega <tomous.wega@gmail.com>
2023-01-29 11:16:47 +0100

    npcs seem to work, but not polished
    Took 29 minutes

--- src/main/java/net/trustgames/lobby/npcs/SpawnNPC.java
+++ src/main/java/net/trustgames/lobby/npcs/SpawnNPC.java
@@ -1,16 +1,19 @@
 package net.trustgames.lobby.npcs;
 
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.EntityPlayer;
 import net.trustgames.core.Core;
 import net.trustgames.core.managers.NPCManager;
 import net.trustgames.lobby.Lobby;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.Set;
 
 public class SpawnNPC implements Listener {
 
@@ -31,13 +34,32 @@ public class SpawnNPC implements Listener {
         YamlConfiguration config = YamlConfiguration.loadConfiguration(npcConfig.getNPCFile());
         Player player = event.getPlayer();
 
-        for (String name :config.getConfigurationSection("npcs").getKeys(false)){
+        Set<String> keys = config.getConfigurationSection("npcs").getKeys(false);
+
+        for (String name : keys){
             Location location = config.getLocation("npcs." + name + ".location");
-            World world = location.getWorld();
+            float yaw = location.getYaw();
+            float pitch = location.getPitch();
             String texture = config.getString("npcs." + name + ".texture");
             String signature = config.getString("npcs." + name + ".signature");
 
-            ServerPlayer npc = npcManager.create(location, name);
+            EntityPlayer npc = npcManager.create(location, name);
+            npcManager.hideName(npc);
+            npcManager.add(npc, player);
+
+            Bukkit.getScheduler().runTaskLater(lobby, () -> {
+                npcManager.skin(npc, player, texture, signature);
+                npcManager.look(npc, player, yaw, pitch, true);
+
+                Bukkit.getScheduler().runTaskLater(core, () -> npcManager.hideTab(npc, player), 30);
+
+            }, 50);
         }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event){
+        Player player = event.getPlayer();
+
     }
 }
